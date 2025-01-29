eb87ecf57a2afa603278919fe8f145c8646ad556
zmaguschpe <zmaguschpe@outlook.de>
2025-01-29 19:59:57 +0100

    base3

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/")
+@app.route("/index")
+def home():
+    return render_template("index.html")
+
+@app.route('/st')
+def showzm():
+	return 'zm'
+
+@app.route("/<name>")
+def user(name):
+	return render_template('i1.html', name=name)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
