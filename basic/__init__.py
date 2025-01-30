e84460bf9655d2c04b1a4ba2f4b1c1aa57d97e23
zmaguschpe <zmaguschpe@outlook.de>
2025-01-30 09:46:36 +0100

    struct config

--- basic/__init__.py
+++ basic/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('config')
 
     from .basic import basic
     app.register_blueprint(basic, url_prefix='/basic')
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = app.config["DEBUG"])
 
 
