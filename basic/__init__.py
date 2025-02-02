c938304231613a0fde0db482d16dc27d5d2044e1
zmaguschpe <zmaguschpe@outlook.de>
2025-02-02 08:06:34 +0100

    import _ init model

--- basic/__init__.py
+++ basic/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-    from .models import Info
+    
 
 
     from .bpinput.views import bpinput
