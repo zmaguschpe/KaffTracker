7e9fca8e80bef5aa6e283f76a67f64a761be638a
zmaguschpe <zmaguschpe@outlook.de>
2025-02-02 08:03:19 +0100

    kleinkram

--- basic/bpshow/views.py
+++ basic/bpshow/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template#,  request, redirect, url_for
 from ..models import Info
 from .. import db
 
