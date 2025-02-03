82fa25caf9e07c24aa2815e24b68641eca614c5a
zmaguschpe <zmaguschpe@outlook.de>
2025-02-03 17:58:45 +0100

    navbar kleinkram

--- basic/auth/auth.py
+++ basic/auth/auth.py
@@ -13,6 +13,7 @@ def login():
 # if current_user not loged in
     form = LoginForm()
     if request.method == 'POST'and form.validate_on_submit():
+        #sinn??
         username = form.username.data
         password = form.password.data
         user = User.query.filter_by(username=username).one()
