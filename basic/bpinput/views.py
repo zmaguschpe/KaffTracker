12a192476af3d2938a3c6382c435a7c5295d03a2
zmaguschpe <zmaguschpe@outlook.de>
2025-02-03 21:34:52 +0100

    db fix

--- basic/bpinput/views.py
+++ basic/bpinput/views.py
@@ -2,15 +2,17 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from ..models import Info
 from .. import db
 from .forms import EntryForm
+from flask_login import login_required, current_user
 
 bpinput = Blueprint('bpinput', __name__, template_folder='templates')
 
 @bpinput.route('/', methods=['GET', 'POST'])
 @bpinput.route('/input', methods=['GET', 'POST'])
+@login_required
 def entry():
     form = EntryForm()
     if form.validate_on_submit():
-        new_info = Info(data=form.info.data) 
+        new_info = Info(data=form.info.data, user_id=current_user.id)
         db.session.add(new_info) 
         db.session.commit()
         return redirect(url_for('bpinput.entry'))
--- basic/bpshow/views.py
+++ basic/bpshow/views.py
@@ -1,14 +1,17 @@
-from flask import Blueprint, render_template#,  request, redirect, url_for
+from flask import Blueprint, render_template #,  request, redirect, url_for
 from ..models import Info
 from .. import db
+from flask_login import login_required, current_user
 
 bpshow = Blueprint('bpshow', __name__, template_folder='templates')
 
 
 @bpshow.route('/show')
+@login_required
 def show():
-    found_info = Info.query.all()
-    return render_template('show.html', info=found_info)
+    user=current_user
+
+    return render_template('show.html', user=current_user)
 
 
   
